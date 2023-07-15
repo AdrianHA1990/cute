26b377628db2d127e826f0b54a26a802c3a8423a
mi nombre <adrian.arellano0304@gmail.com>
2023-07-14 23:37:25 -0600

    finaltres

--- proyecto_cute/settings.py
+++ proyecto_cute/settings.py
@@ -76,7 +76,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(SETTINGS_PATH,'templates')],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
