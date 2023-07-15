887b121317dbf411ea3575862dde600733294c06
mi nombre <adrian.arellano0304@gmail.com>
2023-07-14 20:41:09 -0600

    tres

--- Cute_paginas/urls.py
+++ Cute_paginas/urls.py
@@ -44,4 +44,8 @@ urlpatterns = [
     path('porta/', Porta.as_view(), name="porta"),
     
     
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
