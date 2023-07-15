8ccf4db76e756a27f7e5d9b753b99bb02dea6b06
mi nombre <adrian.arellano0304@gmail.com>
2023-07-14 20:24:48 -0600

    uno

--- proyecto_cute/urls.py
+++ proyecto_cute/urls.py
@@ -21,6 +21,9 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', include('Cute_paginas.urls'),),
+    path('', include('Cute_paginas.urls'),),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
