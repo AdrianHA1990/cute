f84ee5720486789c8f60544a3f6661b2ce1e6afd
mi nombre <adrian.arellano0304@gmail.com>
2023-07-14 22:02:17 -0600

    siete

--- Cute_paginas/urls.py
+++ Cute_paginas/urls.py
@@ -7,7 +7,7 @@ from django.contrib.staticfiles.urls import static
 app_name = "app_cute"
 
 urlpatterns = [
-    path('inicio/', Inicio.as_view(), name="Paginas_Base/inicio.html"),
+    path('', Inicio.as_view(template_name="Paginas_Base/inicio.html")),
     path('cajas/', Cajas.as_view(), name="cajas"),
     path('arreglos/', Arreglos.as_view(), name="arreglos"),
     path('fotos/', Fotos.as_view(), name="fotos"),
